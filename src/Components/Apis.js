1dbcd3e69e9e4d7d87ea7a6d010fe875ca8cda60
Rig Digital <outsourcingtechrg@gmail.com>
2026-04-24 07:26:27 +0500

    refactor(Apis): remove commented-out application API endpoints for clarity

--- src/Components/Apis.js
+++ src/Components/Apis.js
@@ -37,16 +37,6 @@ export  const API = {
   DeleteShift: (id) => `${mainOrigin}/shifts/${id}`, //linked
   UpdateShifts: (id) => `${mainOrigin}/shifts/${id}`, //linked
   shiftDetailsById: (id) => `${mainOrigin}/shifts/employee/${id}`, //linked
-  
-  // applications
-// GetAllApplications:  `${mainOrigin}/applications`,
-// ApplicationStats:    `${mainOrigin}/applications/stats`,
-// ApplicationById:     (id) => `${mainOrigin}/applications/${id}`,
-// CreateApplication:   `${mainOrigin}/applications`,
-// HODAction:           (id) => `${mainOrigin}/applications/${id}/hod`,
-// HRAction:            (id) => `${mainOrigin}/applications/${id}/hr`,
-// UpdateApplication:   (id) => `${mainOrigin}/applications/${id}`,
-// DeleteApplication:   (id) => `${mainOrigin}/applications/${id}`,
 
   // Attendance Sync
   ListAttendnaceSync: `${mainOrigin}/attendance/sync/`, //linked
